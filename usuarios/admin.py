be580a3c121510a65727b7e526f6d6d0cf6063a3
sebas <sebas@sebas.sebas>
2025-06-05 11:20:07 +0200

    Comentamos toda la app usuarios

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,6 +1,11 @@
+"""Configuración del panel de administración para la app de usuarios."""
+
 from django.contrib import admin
 from .models import Perfil
 
 @admin.register(Perfil)
 class PerfilAdmin(admin.ModelAdmin):
+    """Ajustes para mostrar los perfiles en el admin."""
+
+    # Campos que se mostrarán en el listado
     list_display = ["user", "rol", "filtro_adulto"]
--- usuarios/apps.py
+++ usuarios/apps.py
@@ -1,6 +1,10 @@
+"""Configuración de la aplicación `usuarios`."""
+
 from django.apps import AppConfig
 
 
 class UsuariosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usuarios'
+    """Clase de configuración para registrar la app en Django."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "usuarios"

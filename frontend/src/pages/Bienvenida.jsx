98ebfaa95647d0c41905eaa26afcbb493463f5c4
sebas <sebas@sebas.sebas>
2025-06-05 13:15:32 +0200

    Configuraciones y más traducciones

--- frontend/src/pages/Bienvenida.jsx
+++ frontend/src/pages/Bienvenida.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useLang } from "../context/LangContext";
 
 export default function Bienvenida() {
   const [usuario, setUsuario] = useState("");
   const navigate = useNavigate();
+  const { t } = useLang();
 
   useEffect(() => {
     fetch("/api/usuarios/session", {
@@ -21,7 +23,7 @@ export default function Bienvenida() {
 
   return (
     <div className="w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl">
-      <h1 className="text-3xl font-bold text-center">¡Bienvenido, {usuario}!</h1>
+      <h1 className="text-3xl font-bold text-center">{t("welcome")} {usuario}!</h1>
     </div>
   );
 }
